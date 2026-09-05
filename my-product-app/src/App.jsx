812060fcf5b5bafb60e1aa9ebd4b8254a701f8fb
장인호 <wphoya@gmail.com>
2026-09-06 00:39:48 +0900

    라우터 수정

--- my-product-app/src/App.jsx
+++ my-product-app/src/App.jsx
@@ -1,16 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom'; // 💡 BrowserRouter 대신 HashRouter 사용
 import UserPage from './pages/UserPage';
 import AdminPage from './pages/AdminPage';
 
 function App() {
   return (
-    /* 💡 핵심: 깃허브 배포 주소의 하위 경로명을 basename으로 명시해 줍니다. */
-    <BrowserRouter basename="/my-product-app">
+    /* HashRouter를 쓰면 깃허브 하위 경로 꼬임 문제(basename)가 원천 차단됩니다. */
+    <HashRouter>
       <Routes>
+        {/* 일반 주소: https://github.io */}
         <Route path="/" element={<UserPage />} />
+        
+        {/* 관리자 주소: https://github.io#/admin */}
         <Route path="/admin" element={<AdminPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
