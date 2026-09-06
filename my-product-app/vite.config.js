73250a5ecf851189913e81ef341fd34278a32eb5
장인호 <wphoya@gmail.com>
2026-09-06 10:20:44 +0900

    홈페이지 및 베이스 경로 수정

--- my-product-app/vite.config.js
+++ my-product-app/vite.config.js
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev
 export default defineConfig({
-  base: "/JMC/my-product-app/", 
+  base: "/JMC/my-product-app", 
+  // base: "./", 
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist', // 결과물이 dist 폴더로 명확히 가도록 고정
